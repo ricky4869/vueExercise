f2a57e29f6b1d0296d5864f28c3f494738eb023e
Ricky Wu <zm9poiuy@gmail.com>
2020-09-10 16:47:00 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -14,10 +14,9 @@ import './bus';
 import filterCurrency from './filters/currency'
 import flyIn from './mixin/flyin'
 
-
+axios.defaults.withCredentials = true;
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
-axios.defaults.withCredentials = true;
 
 Vue.component('ValidationProvider', ValidationProvider);
 Vue.component('ValidationObserver', ValidationObserver);
